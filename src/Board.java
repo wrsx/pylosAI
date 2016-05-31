9d0a3109e3025411b847b0b2340577d5e2ac62fc
tankers746 <tankers746@live.com>
2016-06-01 02:04:59 +0800

    Created board

---
+++ src/Board.java
@@ -0,0 +1,28 @@
+
+import java.util.ArrayList;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Tom
+ */
+public class Board {
+    int[][] level0;
+    int[][] level1;
+    int[][] level2;
+    int level3;
+    
+    
+    public Board() {
+        level0 = new int[4][4];
+        level1 = new int[3][3];
+        level1 = new int[2][2];
+        level3 = 0;
+    }
+    
+}
