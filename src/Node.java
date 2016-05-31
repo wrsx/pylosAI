8afade5986841e0e3b4f795b5478d157cc99a4f3
wrsx <mewithmyclub@hotmail.com>
2016-06-01 01:51:23 +0800

    init

---
+++ src/Node.java
@@ -0,0 +1,9 @@
+
+
+/**
+ *
+ * @author Jason
+ */
+public class Node {
+    
+}
---
+++ src/Pylos.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class Pylos {
+
+    public int whiteSpheres;
+    public int blackSpheres;
+    
+    public Pylos() {
+        whiteSpheres = 15;
+        blackSpheres = 15;
+    }
+    
+    public void idle() {
+        
+        Scanner user_input = new Scanner(System.in);
+        
+        while (true) {
+            System.out.println("Please select a valid move: ");
+                try {
+                    move(user_input.next());
+                }
+                catch (Exception e) {
+                    e.printStackTrace();
+                }
+        }
+    }
+    
+    public void move(String arg) {
+        System.out.println("You chose move: " + arg);
+    }
+    
+    public static void main(String[] args) {
+        Pylos game = new Pylos();
+        game.idle();
+    }
+    
+}
