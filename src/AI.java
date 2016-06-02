ffb58999bd127feeed2ce86590c9483ad30fcc3a
tankers746 <tankers746@live.com>
2016-06-02 09:44:38 +0800

    Fixed AI

--- src/AI.java
+++ src/AI.java
@@ -1,7 +1,10 @@
 
+import java.util.ArrayList;
+
+
 public class AI {
-        final static boolean MAX = true;
-        final static boolean MIN = false;
+        final static int MAX = 1;
+        final static int MIN = -1;
         Board node;
         Board bestMove;
         int depth = 4;
@@ -10,14 +13,14 @@ public class AI {
                 node = board;
         }
         /*Returns the score of the best move*/
-        public int alphaBeta(Board node, int depth, int a, int b, boolean player) {
+        public int alphaBeta(Board node, int depth, int a, int b, int player) {
                 if (depth == 0 || node.gameFinished()) {
-                        return node.Score();
+                        return node.getBoardScore();
                 }                
-                ArrayList<Board> childNodes = node.getPosibleBoards();
+                ArrayList<Board> childNodes = node.getPossibleBoards(player);
                 if (player ==  MAX) {
                             for (int i = 0; i < childNodes.size(); i++) {
-                                    a = max(a, alphaBeta(childNodes[i], depth - 1, a, b, !player), node, childNodes[i]);
+                                    a = max(a, alphaBeta(childNodes.get(i), depth - 1, a, b, -player), node, childNodes.get(i));
                                     if (b <= a) {
                                             break;
                                     }
@@ -26,7 +29,7 @@ public class AI {
                 }
                 else {
                         for (int i = 0; i < childNodes.size(); i++) {
-                                b = min(b, alphaBeta(childNodes[i], depth - 1, a, b, !player), node, childNodes[i]);
+                                b = min(b, alphaBeta(childNodes.get(i), depth - 1, a, b, -player), node, childNodes.get(i));
                                 if (b <= a) {
                                         break;
                                 }
